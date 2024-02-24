c766bfefdb1a9acf9cad09f5b5246a3a528e75fb
Yuushin <Shyy>
2024-02-24 10:16:17 +0800

    update the styles

--- src/app/airdrop/[id]/page.tsx
+++ src/app/airdrop/[id]/page.tsx
@@ -4,6 +4,8 @@ import { AirDropData } from "@/libs/types";
 import { Footer } from "@/views/Footer";
 import TitleBar from "@/views/TitleBar";
 import Image from "next/image";
+import styles from "./page.module.css";
+import { formatCurrency, formatTokenAmount } from "@/libs/utils";
 
 export default async function Page({ params }: {
 	params: { id: string };
@@ -22,33 +24,38 @@ export default async function Page({ params }: {
 
 				<div
 					className="articleView">
-					<div className="flex-column-align-left">
-						<Image
-							width={64}
-							height={64}
-							alt={airdrop.title}
-							src={airdrop.logo}
-							className="circleImage" />
+					<div className={styles.infoBox}>
+						<div className="flex-column-align-left">
+							<Image
+								width={64}
+								height={64}
+								alt={airdrop.title}
+								src={airdrop.logo}
+								className="circleImage" />
 
-						{airdrop.asset && <>
-							<div className="flex-row-align-left">
-								<div className="label">token:</div>
-								<div>{airdrop.asset.symbol}</div>
-							</div>
+							{airdrop.asset && <>
+								<div className="flex-row-align-left">
+									<div className="label">token:</div>
+									<div>{airdrop.asset.symbol}</div>
+								</div>
+							</>}
 
-							<div className="flex-row-align-left">
+							{airdrop.amount && <div className="flex-row-align-left">
 								<div className="label">amount:</div>
-								<div>{airdrop.amount}&nbsp;{airdrop.asset.symbol}(${airdrop.value})</div>
-							</div>
-						</>}
+								<div>{formatTokenAmount(airdrop.amount)}</div>
+							</div>}
 
-						{airdrop.start && airdrop.end && <div className="flex-row-align-left">
-							<div>{new Date(airdrop.start).toLocaleDateString()}</div>
-							<div>-</div>
-							<div>{new Date(airdrop.end).toLocaleDateString()}</div>
-						</div>}
+							{airdrop.value && <div className="flex-row-align-left">
+								<div className="label">value:</div>
+								<div>{formatCurrency(airdrop.value)}</div>
+							</div>}
 
-						<p>&nbsp;</p>
+							{airdrop.start && airdrop.end && <div className="flex-row-align-left">
+								<div>{new Date(airdrop.start).toLocaleDateString()}</div>
+								<div>-</div>
+								<div>{new Date(airdrop.end).toLocaleDateString()}</div>
+							</div>}
+						</div>
 
 						<a
 							className="linkButton"
